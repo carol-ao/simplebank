b487ae3ccf9e8aec2c16f521667e6f8acbbf7a78
carolina-ao <carolina.oliveira@petlove.com.br>
2022-09-22 10:51:34 -0300

    first commit

---
+++ src/main/java/com/carol/simplebank/SimplebankApplication.java
@@ -0,0 +1,13 @@
+package com.carol.simplebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carol/simplebank/SimplebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carol.simplebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
